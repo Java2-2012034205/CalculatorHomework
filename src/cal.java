ab87768bae16b367118f658d21ce4fde5c21310b
김민섭 <can0306@knu.ac.kr>
2016-06-19 23:33:31 +0900

    second hotfix commit

--- src/cal.java
+++ src/cal.java
@@ -10,11 +10,11 @@ import javax.swing.JPanel;
 
 public class cal extends JFrame {
     JLabel label;
-    JButton bNum[] = new JButton[10];	
-    JButton plus, minus, multi, div, equal, mod, clear; 
+    JButton bNum[] = new JButton[10];
+    JButton plus, minus, multi, div, equal, mod, clear;
 
-    String inputValue;	
-    int result;		
+    String inputValue;
+    int result;	
     char lastOp;		
     public static void main(String[] args) {
         new cal();
@@ -22,7 +22,7 @@ public class cal extends JFrame {
 
     public cal() {
         super("Calcurator");
-        setBounds(300, 300, 300, 300);
+        setBounds(300, 300, 300, 300);	
         setResizable(false);	
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         label = new JLabel("0", JLabel.RIGHT);
@@ -38,25 +38,42 @@ public class cal extends JFrame {
         clear = new JButton("C");
 
         int i;
-        for( i=0;i<bNum.length;i++ ) {		
-        	bNum[i] = new JButton(Integer.toString(i));
+        for( i=0;i<bNum.length;i++ ) {	
+            bNum[i] = new JButton(Integer.toString(i));
         }
         p.add(clear); p.add(bNum[7]); p.add(bNum[8]); p.add(bNum[9]); 
         p.add(multi);  p.add(div); p.add(bNum[4]); p.add(bNum[5]); p.add(bNum[6]);
         p.add(plus); p.add(minus);  p.add(bNum[1]); p.add(bNum[2]); p.add(bNum[3]); 
         p.add(mod);   p.add(equal);  p.add(bNum[0]);   
+      
+        
+        NumberHandler nh = new NumberHandler();
+        for( i=0;i<bNum.length;i++ ) {
+            bNum[i].addActionListener(nh);	
+        }
 
+        CalcHandler ch = new CalcHandler();	
+        plus.addActionListener(ch);
+        minus.addActionListener(ch);
+        multi.addActionListener(ch);
+        div.addActionListener(ch);
+        equal.addActionListener(ch);
+        mod.addActionListener(ch);
+        clear.addActionListener(ch);
+        setVisible(true);
     }
+
+
     class NumberHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String s = e.getActionCommand();
             if(inputValue == null) {	
-                if( s.equals("0")) 
+                if( s.equals("0")) 	
                    return ;
                 else inputValue = new String();	
 
             }
-            if( inputValue.length() >= 9)	  
+            if( inputValue.length() >= 9)	
                return;
             inputValue = inputValue + s;  
             label.setText(inputValue);  
@@ -77,10 +94,9 @@ public class cal extends JFrame {
                 return ;
             }
 
-            if( inputValue != null ) {
-		
+            if( inputValue != null ) {	
                 value = Integer.parseInt(inputValue);	
-                switch(lastOp) {	  
+                switch(lastOp) {	 
                     case '+': 
                        result = result + value;
                        break;
@@ -100,10 +116,11 @@ public class cal extends JFrame {
                        result = value;
                        break;
                 }
-                label.setText(Integer.toString(result));  
+                label.setText(Integer.toString(result)); 
             }
-            inputValue = null;	
+            inputValue = null;
             lastOp = source.getText().charAt(0);
         }
     }
 }
+
